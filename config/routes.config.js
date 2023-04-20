9d670fbfb12c4fa88b66cb87a22ec691b867c8a6
Edson <eddyao1990@gmail.com>
2023-04-20 04:40:51 +0200

    cleaning project

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,7 +3,6 @@ const usersController = require('../controllers/user.controller');
 const authController = require('../controllers/auth.controller');
 const { isAuthenticaded } = require('../middlewares/auth.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
-const postsController = require('../controllers/posts.controller');
 const pinController = require('../controllers/pin.controller');
 const followController = require('../controllers/follow.controller');
 const fileUploader = require('../config/cloudinary.config');
@@ -35,13 +34,6 @@ router.post('/users/edit', fileUploader.single('image'), usersController.edit);
 router.post('/map/pin', pinController.create);
 router.get('/map/pin', pinController.list);
 
-//Scrapbook
-
-router.get('/posts', authMiddleware.isAuthenticaded, postsController.getPost);
-router.post('/posts', authMiddleware.isAuthenticaded, postsController.createPost);
-router.get('/posts/:id', authMiddleware.isAuthenticaded, postsController.getPost);
-router.patch('/posts/:id', authMiddleware.isAuthenticaded, postsController.updatePost);
-router.delete('/posts/:id', authMiddleware.isAuthenticaded, postsController.deletePost);
 
 //Follow
 router.post('/follow/:id', authMiddleware.isAuthenticaded, followController.doFollow);
